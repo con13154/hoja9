37281c7667d725177d86430e0a57b9ef5e255ade
L2Dev <orellanasolares.luis@gmail.com>
2014-10-10 21:38:44 -0600

    Creacion de proyecto

--- HojadeTrabajo8/src/hojadetrabajo8/Word.java
+++ HojadeTrabajo8/src/hojadetrabajo8/Word.java
@@ -1,57 +1,59 @@
 /*
-UVG
-Algoritmos y Estructuras de Datos - 2011
-Hoja de trabajo 7 
-Autor: Eduardo Castellanos
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-Descripci�n: Word. Clase para almacenar las palabras junto con su tipo. 
-*/
 package hojadetrabajo8;
-class Word implements Comparable<Word> {
-	private String word;
-	private String type;
-	
-	// Constructor, inicializa la palabra con su tipo
-	public Word(String word, String type)
-	{
-		this.word=word;
-		this.type=type;
-	}
-	
-	public Word()
-	{
-		this.word= "";
-		this.type="";
-	}
-	
-	// Comparadores, solo importa comparar la palabra, no el tipo.
-	public int compareTo(Word o)
-	{
-		return this.word.compareTo(o.getWord());
-	}
-	
-	public boolean equals(Object obj)
-	{
-		return (obj instanceof Word && getWord().equals(((Word)obj).getWord()));
-	}
-	
-	// M�todos de acceso..
-	public void setWord(String word)
-	{
-		this.word=word;
-	}
-	public void setType(String type)
-	{
-		this.type=type;
-	}
-	public String getWord()
-	{
-		return word;
-	}
-	
-	public String getType()
-	{
-		return type;
-	}
-	
+
+/**
+ *
+ * @author E. de Mata
+ */
+public class Word implements Comparable<Word>{
+    private String word;
+        private String type;
+        
+        // Constructor, inicializa la palabra con su tipo
+        public Word(String word, String type)
+        {
+                this.word=word;
+                this.type=type;
+        }
+        
+        public Word()
+        {
+                this.word= "";
+                this.type="";
+        }
+        
+        // Comparadores, solo importa comparar la palabra, no el tipo.
+        public int compareTo(Word o)
+        {
+                return this.word.compareTo(o.getWord());
+        }
+        
+        public boolean equals(Object obj)
+        {
+                return (obj instanceof Word && getWord().equals(((Word)obj).getWord()));
+        }
+        
+        // Métodos de acceso..
+        public void setWord(String word)
+        {
+                this.word=word;
+        }
+        public void setType(String type)
+        {
+                this.type=type;
+        }
+        public String getWord()
+        {
+                return word;
+        }
+        
+        public String getType()
+        {
+                return type;
+        }
 }
